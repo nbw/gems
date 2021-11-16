fd510d5cc8641b337666118004a5379a49279b1c
Nathan Willson <willson.b.nathan@gmail.com>
2021-11-16 20:39:29 +0900

    mix: uuid

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule GEMS.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:elixir_uuid, "~> 1.2"}
     ]
   end
 
