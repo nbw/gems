aa94ec613cc1f2ef49d0cc34ab8311419b634d4b
David Hainlin <dhainlin@teladochealth.com>
2022-01-03 17:45:05 -0600

    fix: Tweaked setup to include assets (#4)

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule GEMS.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "cmd --cd assets npm install"],
+      setup: ["deps.get", "cmd --cd assets npm install"],
       "assets.deploy": [
         "cmd --cd assets npm run deploy",
         "esbuild default --minify",
